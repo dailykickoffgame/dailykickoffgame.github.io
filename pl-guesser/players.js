0da847a61278b99601bb004a5af00918e04f9d4e
dailykickoffgame <zelffi@gmail.com>
2026-04-06 15:11:08 +0300

    Update players.js

--- pl-guesser/players.js
+++ pl-guesser/players.js
@@ -1,5 +1,17 @@
 const players = [
- "messi","ronaldo","haaland","salah","mbappe","neymar",
- "de bruyne","kane","bellingham","vinicius",
- "osimhen","foden","saka","rashford","son"
-];
+  {name:"messi", team:"inter miami", league:"mls"},
+  {name:"ronaldo", team:"al nassr", league:"saudi"},
+  {name:"haaland", team:"man city", league:"premier league"},
+  {name:"salah", team:"liverpool", league:"premier league"},
+  {name:"mbappe", team:"psg", league:"ligue 1"},
+  {name:"neymar", team:"al hilal", league:"saudi"},
+  {name:"de bruyne", team:"man city", league:"premier league"},
+  {name:"kane", team:"bayern", league:"bundesliga"},
+  {name:"bellingham", team:"real madrid", league:"la liga"},
+  {name:"vinicius", team:"real madrid", league:"la liga"},
+  {name:"osimhen", team:"napoli", league:"serie a"},
+  {name:"foden", team:"man city", league:"premier league"},
+  {name:"saka", team:"arsenal", league:"premier league"},
+  {name:"rashford", team:"man united", league:"premier league"},
+  {name:"son", team:"tottenham", league:"premier league"}
+]
